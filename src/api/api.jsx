01ebcdb43ee351e84df4d0a1dacc8b0af0ff13f1
Dmitriy <dyuzef@mail.ru>
2022-05-18 19:16:46 +0300

    add API

---
+++ src/api/api.jsx
@@ -0,0 +1,19 @@
+import { API_URL } from '../config/config';
+
+const getMealById = async (mealId) => {
+  const response = await fetch(API_URL + 'lookup.php?i=' + mealId);
+  const meal = await response.json();
+  return meal;
+};
+
+const getAllCategories = async () => {
+  const response = await fetch(API_URL + 'categories.php');
+  return  await response.json();
+}
+
+const getFilerCategories = async (catName) => {
+  const response = await fetch(API_URL + 'filter.php?c=' + catName);
+  return  await response.json();
+}
+
+export {getMealById, getAllCategories}
